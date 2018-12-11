789750fc185572b0131a025f8f4121b8041d0f80
Ryan Kretch <rkretch91@gmail.com>
2018-12-11 13:36:02 +0100

    error handling changed:

--- app-promises.js
+++ app-promises.js
@@ -1,40 +1,35 @@
-const yargs = require('yargs');
 const axios = require('axios')
+const yargs = require('yargs')
 
 const argv = yargs
   .options({
     a: {
       demand: true,
       alias: 'address',
-      describe: 'Address to fetch weather for',
+      description: 'Address to fetch weather for',
       string: true
     }
   })
   .help()
-  .alias('help', 'h')
-  .argv;
+  .argv
 
-var encodedAddress = encodeURIComponent(argv.address);
-var geocodeUrl = `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}&key=AIzaSyBmED3bBLrIOIN2086bbwDlfgeGyq63rKU`
-
+const encodedAddress = encodeURIComponent(argv.address)
+const geocodeUrl = `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}&key=AIzaSyBmED3bBLrIOIN2086bbwDlfgeGyq63rKU`
 
 axios.get(geocodeUrl).then((response) => {
-  if (response.data.status === 'ZERO_RESULTS') {
+  if (response.data.status === "ZERO_RESULTS") {
     throw new Error('Unable to find that address')
-  }
-  var lat = response.data.results[0].geometry.location.lat
-  var lng = response.data.results[0].geometry.location.lng
-  var weatherUrl = `https://api.darksky.net/forecast/734bca71df3ce034dc365f8ba7485956/${lat},${lng}`
+  } 
+  let lat = response.data.results[0].geometry.location.lat
+  let lng = response.data.results[0].geometry.location.lng
   console.log(response.data.results[0].formatted_address)
+  const weatherUrl = `https://api.darksky.net/forecast/734bca71df3ce034dc365f8ba7485956/${lat},${lng}`
+  console.log(lng)
   return axios.get(weatherUrl)
 }).then((response) => {
-  var temperature = response.data.currently.temperature
-  var apparentTemperature = response.data.currently.apparentTemperature
-  console.log(`It is currently: ${temperature} but feels like ${apparentTemperature}`)
-}).catch((e) => {
-  if (e.code === "ENOTFOUND") {
-    console.log('Unable to connect to API Servers')
-  } else {
-    console.log(e.message)
-  }
+  let temp = response.data.currently.temperature
+  let sum = response.data.currently.summary
+  console.log(`Today is ${sum}. The temperature is ${Number(((temp - 32) / 1.8).toFixed(2))} degrees Celsius`)
+}).catch((error) => {
+  console.log(error.message)
 })
